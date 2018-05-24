569d91660aea3ac7834ee0118cb5c6655f9bff21
antenorm <antenorm@rose-hulman.edu>
2018-05-24 13:19:42 -0400

    problem1

--- src/problem1.py
+++ src/problem1.py
@@ -2,9 +2,9 @@
 Final exam, problem 1.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  May 2018.
+         their colleagues and Ryan Antenore.  May 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # Done# : 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -19,8 +19,8 @@ def main():
     print()
     print('Un-comment the calls in MAIN one by one')
     print(' to run the testing code as you complete the TODOs.')
-    # run_test_problem1a()
-    # run_test_problem1b()
+    run_test_problem1a()
+    run_test_problem1b()
 
 
 def run_test_problem1a():
@@ -89,8 +89,11 @@ def problem1a(numbers):
     Type hints:
       :type numbers:  [int]
     """
+    middle = len(numbers) // 2
+
+    return numbers[0] + numbers[middle] + numbers[len(numbers) - 1]
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # Done: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
 
@@ -165,6 +168,11 @@ def problem1b(numbers):
     Type hints:
       :type numbers:  [int]
     """
+    length = len(numbers) // 3
+    total = 0
+    for k in range(length):
+        total += numbers[length + k]
+    return total
     # -------------------------------------------------------------------------
     # TODO: 3. Implement and test this function.
     #          Tests have been written for you (above).
